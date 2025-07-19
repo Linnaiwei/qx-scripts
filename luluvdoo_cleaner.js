ec6b19ab0fef3b50366f1d5e017a5fbd5a553d80
Hadwin <linnaiwei@gmail.com>
2025-07-20 00:58:39 +0800

    Update luluvdoo_cleaner.js

--- luluvdoo_cleaner.js
+++ luluvdoo_cleaner.js
@@ -1,21 +1,9 @@
 if ($response.body) {
   let body = $response.body;
 
-  // 移除含有恶意指纹的内联脚本
+  // 先只保留一条看看效果
   body = body.replace(
     /<script[^>]*>[^<]*ChmaorrCfozdgenziMrattShzzy[^<]*<\/script>/gi,
-    ''
-  );
-
-  // 移除引用了 lulu-row1.com 的 script 标签
-  body = body.replace(
-    /<script[^>]+src=["'][^"']*lulu-row1\.com[^"']*["'][^>]*><\/script>/gi,
-    ''
-  );
-
-  // 移除包含广告拦截提示函数的脚本
-  body = body.replace(
-    /<script[^>]*>[^<]*function\s+showADBOverlay\(\)[\s\S]*?<\/script>/gi,
     ''
   );
 
